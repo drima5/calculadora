7fd795b956c97894637ee831aeae941396fb791b
drima5 <riosadrimaria91@gmail.com>
2024-06-16 15:22:32 -0500

    cambio de codigo

--- script.js
+++ script.js
@@ -27,11 +27,33 @@ function dividir() {
   document.getElementById('resultado').innerText = resultado;
 }
 
-function cleaner(){
-  document.getElementById('num1').value="";
-  document.getElementById('num2').value="";
-  document.getElementById('resultado').innerHTML="";
+function potencia() {
+// ingresa aqui tu codigo.
+// ingresa aqui tu codigo.
+// ingresa aqui tu codigo. ¡¡¡pista!!!,asi se realizala potencia: Math.pow(num1, num2):
+document.getElementById('resultado').innerText=resultado;
 }
+
+function raiz(){
+    var num1 = parseFloat(document.getElementById('num').value);
+    var resultado = Math.sqrt(num1);
+    document.getElementById('resultado').innerText =resultado;
+}
+
+function modulo(){
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var resultado = num1 % num2;
+    document.getElementById('resultado').innerText = resultado;
+}
+
+function cleaner(){
+    document.getElementById('num1').value="";
+    document.getElementById('num2').value="";
+    document.getElementById('resultado').innerHTML="";
+}
+
+
 
 
 
